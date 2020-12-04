fdd4895029d0562f464d6a295ff07d98b87930d9
Eric Henrichs <erhenri@siue.edu>
2020-12-04 16:14:35 -0600

    Getters and Setters for Person.cs 12/4/2020

--- cs325-coreproject/Person.cs
+++ cs325-coreproject/Person.cs
@@ -10,5 +10,34 @@ namespace cs325_coreproject
     {
         string firstName;
         string lastName;
+
+
+        public string getFirstName() {
+            return firstName;        
+        }
+        
+        public string getLastName() {
+            return lastName;
+        }
+
+        public string getFullName() {
+            return string.Format("{0} {1}", this.firstName, this.lastName);
+        }
+
+
+        public void setFirstName(string name) {
+            this.firstName = name;
+        }
+
+        public void setLastName(string name) {
+            this.lastName = name;
+        }
+
     }
+
+
+    
+
+
+
 }
