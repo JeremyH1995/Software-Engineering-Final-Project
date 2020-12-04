6b60fe9cbe6b2ba23146be8345f92066147511d9
Paris Wood <pwood@siue.edu>
2020-12-04 16:14:35 -0600

    getters and setters

--- cs325-coreproject/Business.cs
+++ cs325-coreproject/Business.cs
@@ -8,6 +8,12 @@ namespace cs325_coreproject
 {
     class Business : Client
     {
-        
+          private string name;
+          private string ein;
+
+        public string getName{ return name;}
+        public string setName(string name){ this.name = name;}
+        public string getEin{ return ein;}
+        public string setEin(string ein){ this.ein = ein;}
     }
 }
