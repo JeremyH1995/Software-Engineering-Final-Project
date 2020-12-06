236a4e5d8bed32a8ba2eb0658ca176da8beaf0dc
Jeremy Heil <jeheil@siue.edu>
2020-12-06 15:35:46 -0600

    added createEmployee form

---
+++ cs325-coreproject/EmployeeLogin.Designer.cs
@@ -0,0 +1,47 @@
+﻿
+namespace cs325_coreproject
+{
+    partial class EmployeeLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // EmployeeLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 324);
+            this.Name = "EmployeeLogin";
+            this.Text = "EmployeeLogin";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
